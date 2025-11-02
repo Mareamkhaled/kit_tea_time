e54a1fa1ca21002890b69b8d0caf3defc49a29e0
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-11-02 21:54:40 +0200

    feat: add CustomSearchButton and ResponsiveColumn widgets for enhanced UI

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -5,6 +5,7 @@ import '../../../../core/utils/app_constants.dart';
 import '../../../../core/utils/app_images.dart';
 import '../../../../core/utils/app_style.dart';
 import '../widgets/breeds_section.dart';
+import '../widgets/custom_search_button.dart';
 import '../widgets/custom_shader_mask.dart';
 import '../widgets/featured_cat_section.dart';
 import '../widgets/info_section.dart';
@@ -15,62 +16,64 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Gap(MediaQuery.of(context).padding.top + 20),
-                Image.asset(AppImages.splashImage, width: 100),
-                const Gap(10),
-                const CustomShaderMask(text: AppConstants.homeTitle),
-                const Gap(10),
-                Text(
-                  textAlign: TextAlign.center,
-                  AppConstants.homeDesc,
-                  style: AppStyle.lemon300Style12Grey,
-                ),
-                const Gap(20),
-                infoSection(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Gap(MediaQuery.of(context).padding.top + 20),
+              Image.asset(AppImages.splashImage, width: 100),
+              const Gap(10),
+              const CustomShaderMask(text: AppConstants.homeTitle),
+              const Gap(10),
+              Text(
+                textAlign: TextAlign.center,
+                AppConstants.homeDesc,
+                style: AppStyle.lemon300Style12Grey,
+              ),
+              const Gap(20),
+              infoSection(),
 
-                const Gap(20),
-                Row(
-                  children: [
-                    CustomShaderMask(
-                      text: "Featured Cats",
-                      style: AppStyle.lemon500Style18White,
+              const Gap(20),
+              Row(
+                children: [
+                  CustomShaderMask(
+                    text: "Featured Cats",
+                    style: AppStyle.lemon500Style18White,
+                  ),
+                ],
+              ),
+              const Gap(20),
+
+              const FeaturedCatsSection(),
+              const Gap(10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomShaderMask(
+                    text: "Popular Breeds",
+                    style: AppStyle.lemon500Style18White,
+                  ),
+
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "View All",
+                      style: AppStyle.lemon300Style12Grey,
                     ),
-                  ],
-                ),
-                const Gap(20),
+                  ),
+                ],
+              ),
 
-                const FeaturedCatsSection(),
-                const Gap(10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomShaderMask(
-                      text: "Popular Breeds",
-                      style: AppStyle.lemon500Style18White,
-                    ),
+              const Gap(10),
+              const BreedsSection(),
+              const CustomSearchButton(),
 
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "View All",
-                        style: AppStyle.lemon300Style12Grey,
-                      ),
-                    ),
-                  ],
-                ),
-
-                const Gap(10),
-                const BreedsSection(),
-              ],
-            ),
+              const Gap(50),
+            ],
           ),
         ),
-      
+      ),
     );
   }
 }
